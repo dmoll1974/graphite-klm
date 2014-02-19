c7b4a6f63de8389ffe48b7e725380ac8debb9c71
dmoll1974 <dmoll1974@gmail.com>
2014-02-19 10:41:27 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,11 @@
-#override['graphite']['listen_port'] = 8080
+override['graphite']['timezone'] = "Africa/Accra"
+override['graphite']['storage_schemas'] = [
+  {
+    'name' => 'everything_30s7d_15m1m',
+    'match-all' => true,
+    'retentions' => '15s:30d,300s:90d'
+  }
+]
+override['graphite']['carbon']['enable_udp_listener'] = "True"
 default[:graphite_klm][:url] = "https://github.com/dmoll1974/graphite-benchmark/archive/master.zip"
 default[:graphite_klm][:home_dir] = "/opt/graphite-benchmark"
