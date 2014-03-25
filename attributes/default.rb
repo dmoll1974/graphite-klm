db1de1ffb19d9a93ef6c05ad6a223fab37113689
dmoll1974 <dmoll1974@gmail.com>
2014-03-25 16:12:23 +0100

    added storage schema for gatling data

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,15 @@
 override['graphite']['timezone'] = "UTC"
 override['graphite']['storage_schemas'] = [
-  {
-    'name' => 'bla',
-    'match-all' => true,
-    'retentions' => '1s:30d,60s:90d'
-  }
+    {
+    'name' => 'Gatling_1s_for_90days',
+    'pattern' => '^gatling\.',
+    'retentions' => '1s:90d'
+  	},
+    {
+    'name' => 'default_15s_for_90days',
+    'pattern' => '.*',
+    'retentions' => '15s:90d'
+  	}
 ]
 override['graphite']['carbon']['enable_udp_listener'] = "True"
 default[:graphite_klm][:url] = "https://github.com/dmoll1974/graphite-benchmark/archive/master.zip"
